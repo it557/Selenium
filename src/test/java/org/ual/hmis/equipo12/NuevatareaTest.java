3ece7da1af783b91af4e1177098ea5c4256b42b0
Ioan Stefan Toderic <72391341+it557@users.noreply.github.com>
2023-06-01 12:18:46 +0200

    FireFox

--- src/test/java/org/ual/hmis/equipo12/NuevatareaTest.java
+++ src/test/java/org/ual/hmis/equipo12/NuevatareaTest.java
@@ -36,7 +36,7 @@ public class NuevatareaTest {
   public void setUp() {
     System.setProperty("webdriver.http.factory", "jdk-http-client");
     // Browser selector
-    int browser = 1; // 0: firefox, 1: chrome,...
+    int browser = 0; // 0: firefox, 1: chrome,...
     Boolean headless = true;
 
     switch (browser) {
@@ -45,7 +45,7 @@ public class NuevatareaTest {
         // Descargar geckodriver de https://github.com/mozilla/geckodriver/releases
         // Descomprimir el archivo geckodriver.exe en la carpeta drivers
 
-        //System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
+        // System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
         FirefoxOptions firefoxOptions = new FirefoxOptions();
         if (headless)
           firefoxOptions.setHeadless(headless);
@@ -57,7 +57,7 @@ public class NuevatareaTest {
         // Descargar Chromedriver de https://chromedriver.chromium.org/downloads
         // Descomprimir el archivo chromedriver.exe en la carpeta drivers
 
-        //System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+        // System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
         ChromeOptions chromeOptions = new ChromeOptions();
         if (headless)
           chromeOptions.setHeadless(headless);
